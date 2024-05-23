59bd5a13054029a0acd581c03554aacbe8350be6
HetHubGit <164913849+HetHubGit@users.noreply.github.com>
2024-05-23 13:12:35 +0100

    Add project files.

---
+++ Day 5th - Zadanie Domowe/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Zadanie Domowe
+        var number = 433422009;
+
+        String numberInSring = number.ToString();
+        char[] numbers = numberInSring.ToArray();
+        
+        char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        foreach (char singular in digits)
+        {
+            var count = 0;
+            foreach (char c in numbers)
+            {
+                if (singular == c) count++;
+            }
+            Console.WriteLine(singular + " => " + count);
+        }
+
+    }
+}
